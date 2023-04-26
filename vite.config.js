4774fc50df539ccc4b33c3fe384ad3178aac3285
kingwell47 <joeldoctor47@gmail.com>
2023-04-26 10:16:46 +0800

    trying to deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const base =
+  process.env.NODE_ENV === "production" ? "/organization.github.io/" : "/";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/cultural-tourism",
+  base,
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    // Ensure the correct public path is used in production.
+    publicPath: base,
+    emptyOutDir: true,
+  },
 });
